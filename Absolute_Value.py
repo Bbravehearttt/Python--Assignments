c57580084c186a5e3ffbfd94d1b2bc73ed7fa3c8
Mkap <murttkapln@gmail.com>
2022-03-24 20:32:59 +0300

    Absolute_Value.py file was added

---
+++ Absolute_Value.py
@@ -0,0 +1,9 @@
+def absolute_value(num) :
+    """This function returns the absolute
+value of the given number."""
+    
+    if num >= 0 :
+        return num
+    
+    else :
+        return -num
