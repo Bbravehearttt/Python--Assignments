c94cc0b645972b81189e1a166fe82f0ac440ef71
Mkap <murttkapln@gmail.com>
2022-04-03 16:36:26 +0300

    Most_Freq_Num.py was added.

---
+++ Most_Freq_Num.py
@@ -0,0 +1,15 @@
+  # Most frequent number
+
+numbers = [1, 3, 7, 4, 3, 0, 3, 6, 3, 3, 4, 4, 4]
+max(numbers)
+a = max(numbers,  key = numbers.count)  # The most frequent number
+b = numbers.count(max(numbers, key = numbers.count))  # How many times repeat it.
+print("The most frequent number is {} and it was {} times repeated".format(a, b))
+
+numbers = [1, 3, 7, 4, 3, 0, 3, 6, 3, 3, 4, 4, 4]
+max(numbers)
+a = max(numbers,  key = numbers.count)  # The most frequent number
+b = numbers.count(max(numbers, key = numbers.count))  # How many times repeat it.
+print("The most frequent number is {} and it was {} times repeated".format(a, b))
+
+
